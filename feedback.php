10ddc6096df0a7444b516a58e797dab8d5f3e168
Sofa <squishysparrow7.7@gmail.com>
2023-05-30 23:39:33 +1200

    Add current stuff

---
+++ feedback.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Baot's Site</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="/css/styles.css" rel="stylesheet">
+    </head>
+    <body>
+        <?php include("header.html"); ?>
+
+        <main>
+        I'm not doing any of that, thanks.
+        </main>
+
+        <?php include("footer.html"); ?>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Baot's Site</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="/css/styles.css" rel="stylesheet">
+    </head>
+    <body>
+        <?php include("header.html"); ?>
+
+        <main>
+<h2>The home page</h2>
+<p>Yep, it's website time.</p>        
+        </main>
+
+        <?php include("footer.html"); ?>
+    </body>
+</html>
