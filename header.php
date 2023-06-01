31639a7e8c1c6d60dee9dab651b897ce8b71abbd
Sofa <squishysparrow7.7@gmail.com>
2023-06-01 18:50:04 +1200

    Grab a random image from the folder to use as a ruler

--- header.php
+++ header.php
@@ -11,5 +11,33 @@
     </ul>
   </nav>
 
-  <hr id="header-end" <?php ?> />
+  <hr id="header-end" <?php
+    // Pick a random image to set --img to
+    $folder = "imgs/ruler/";
+    
+    $files = array();
+
+    // https://www.php.net/manual/en/class.directoryiterator.php
+    // Grab all the pngs
+  foreach (new DirectoryIterator($folder) as $fileInfo) {
+      if ($fileInfo->isDot()) {
+          continue; 
+      }
+
+          $fn = $fileInfo->getFilename();
+
+      if (!preg_match("/.png$/", $fn)) {
+          continue;
+      }
+
+      array_push($files, $fn);
+  }
+
+    $idx = array_rand($files, 1);
+
+    echo 'style="--img: url(\'';  
+    // Make it absolute because the images are outside the css folder
+    echo "/" . $folder . $files[$idx];
+    echo '\')"';
+    ?> />
 </header>
